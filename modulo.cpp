83d90286752e4ca36dd2a96b8cac1b3cd4495999
Nico  Puhlmann <puhlmann7@googlemail.com>
2020-11-12 10:44:14 +0000

    anpassungen

--- modulo.cpp
+++ modulo.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int mod(int number, int modul)
 {
-
+    bool modulIsNegativ = false;
+    if (number == modul || number == (modul * -1) || (number * -1) == modul)
+    {
+        return 0;
+    }
     if (modul < 0)
     {
+        modulIsNegativ = true;
         modul = modul * -1;
     }
     if (number > 0)
@@ -15,25 +20,47 @@ int mod(int number, int modul)
         {
             number -= modul;
         }
-        return number;
+        int solution = modulIsNegativ ? (number * -1) : number;
+        return solution;
     }
     else
     {
+        if (modulIsNegativ)
+        {
+            return mod(number * -1, modul * -1);
+        }
         while (number < 0)
         {
             number += modul;
         }
-        return number;
+        int solution = modulIsNegativ ? (number * -1) : number;
+        return solution;
     }
 }
 
 int main()
 {
     int num, modul;
+    string answer;
     cout << "Geben Sie eine Ganzzahl ein: ";
     cin >> num;
     cout << "Geben Sie den Modul ein: ";
     cin >> modul;
     cout << "Ergebnis: " << mod(num, modul) << endl;
+    cout << "wanna see Solutions? y/n : ";
+    cin >> answer;
+    int compare = answer.compare("y");
+    if (compare == 0)
+    {
+        cout << "87654 mod 13 =" << 87654 % 13 << endl;
+        cout << "-87654 mod 13 =" << -87654 % 13 << endl;
+        cout << "87654 mod -13 =" << 87654 % -13 << endl;
+        cout << "-87654 mod -13 =" << -87654 % -13 << endl;
+        cout << "10 mod 10 =" << 10 % 10 << endl;
+    }
+    else
+    {
+        cout << "Bye!" << endl;
+    }
     return 0;
 }
